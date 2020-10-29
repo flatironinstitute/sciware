03794833a28db9db8bca0c7654e058e5264d5d82
ahbarnett <ahb@math.dartmouth.edu>
2020-10-29 11:56:54 -0400

    examples/rootfind1d.py use tol

--- 12_Functions/examples/rootfind1d.py
+++ 12_Functions/examples/rootfind1d.py
@@ -4,17 +4,18 @@
 # Note scipy.optimize.root does something else!
 
 
-def rootfind1d(f, dfdx, x0):
+def rootfind1d(f, dfdx, x0, tol=1e-9):
     """Find a nearby root of a 1D scalar function.
     Inputs: f - a 1D scalar function
             dfdx - its derivative function
             x0 - an initial guess to the root
+            tol (optional) - desired absolute tolerance in the root
     Returned value: approximate root x, meaning f(x)=0.
     Notes: uses the Newton-Raphson iteration.
     """
     while True:
         xnew = x0 - f(x0)/dfdx(x0)
-        if abs(xnew-x0) < 1e-9:
+        if abs(xnew-x0) < tol:
             break
         x0 = xnew
     return xnew
@@ -24,8 +25,9 @@ if __name__ == "__main__":
     # if this module is simply run, does test: checks root x=pi of f(x)=sin(x) 
     f = lambda x: sin(x)
     dfdx = lambda x: cos(x)
-    x = rootfind1d(f, dfdx, 2.0)
-    if abs(x-pi)<1e-9:
+    tol=1e-12
+    x = rootfind1d(f, dfdx, 2.0, tol=tol)
+    if abs(x-pi)<tol:
         print('pass')
     else:
         print('fail')
