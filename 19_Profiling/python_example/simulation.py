8e0b6cde10fd2d60b64bd456cbce35b47e88a032
Robert Blackwell <rblackwell@scclin016.flatironinstitute.org>
2022-01-12 13:27:55 -0500

    Update simulation to do something useful
    Also switch out write functions to have the same function signature

--- 19_Profiling/python_example/simulation.py
+++ 19_Profiling/python_example/simulation.py
@@ -1,59 +1,63 @@
 import numpy as np
 import os
-import sys
+import glob
 
-n_steps = 100000
-n_write = 100
+n_steps = 10000
+n_write = 10
 n_particles = 1000
 n_dim = 3
-coords = np.random.uniform(size=(n_particles, n_dim))
-
 traj_file = "traj.dat"
+D = 1.0
+dt = 0.01
+kT = 1.0
+gamma = kT / D
+k_spring = 0.1
 
 
-def write_handle(f, pos):
-    f.write(pos)
+def write_handle(pos, traj_file, index=None):
+    # Note: Using static variables like this is not good practice in most cases, but suits for
+    # simplicity in this example to maintain the same function signature. Better practice is to
+    # just pass a file handle in.
+    if not hasattr(write_handle, 'f'):
+        write_handle.f = open(traj_file, 'wb')
+    write_handle.f.write(pos)
 
 
-def write_append(pos, index=None):
+def write_append(pos, traj_file, index=None):
     f = open(traj_file, 'ab')
     f.write(pos)
 
 
-def write_single(pos, index):
+def write_single(pos, traj_file, index):
     f = open(traj_file + "." + str(index), 'wb')
     f.write(pos)
 
 
 def timestep(pos):
-    pos = np.random.uniform(size=pos.shape)
+    dx_diffusion = np.random.normal(loc=0.0,
+                                    scale=np.sqrt(2 * dt * D),
+                                    size=pos.shape)
+    dx_spring = k_spring * (pos[:, 1:-1] - pos[:, 0:-2]) * dt / gamma
+    pos[:, 0:-2] += dx_spring
+    pos[:, 1:-1] -= dx_spring
+    pos += dx_diffusion
 
 
-def simulate(pos, write_fun):
+def cleanup(traj_file):
+    for filepath in glob.glob(traj_file + "*"):
+        os.remove(filepath)
+
+
+def simulate(pos, traj_file, write_fun):
     for i_step in range(n_steps):
         timestep(pos)
         if i_step % n_write == 0:
-            write_fun(pos, i_step // n_write)
+            write_fun(pos, traj_file, i_step // n_write)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Supply writer as argument: handle, append, single")
+    write_fun = write_handle
 
-    if os.path.isfile(traj_file):
-        os.remove(traj_file)
-
-    writer = sys.argv[1]
-    if writer == "handle":
-        f = open(traj_file, 'wb')
-        write_fun = lambda pos, index: write_handle(f, pos)
-    elif writer == "append":
-        write_fun = write_append
-    elif writer == "single":
-        write_fun = write_single
-    else:
-        print(
-            "Invalid writer supplied. Valid options are: handle, append, single"
-        )
-
-    simulate(coords, write_fun)
+    coords = np.zeros(shape=(n_dim, n_particles))
+    cleanup(traj_file)
+    simulate(coords, traj_file, write_append)
