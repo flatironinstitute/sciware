3064fee12bb7c2b9d89340a9def1689cff0222fa
Paul Murray <plmrry@gmail.com>
2023-07-12 14:01:06 -0400

    remove unused js, remove missing image reference

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,11 @@
-/*
-	Strata by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+// @ts-check
 
 (function ($) {
   var $window = $(window),
     $body = $("body"),
     $header = $("#header"),
     $footer = $("#footer"),
-    $main = $("#main"),
-    settings = {
-      // Parallax background effect?
-      parallax: true,
-
-      // Parallax factor (lower = more intense, higher = less intense).
-      parallaxFactor: 20,
-    };
+    $main = $("#main");
 
   // Breakpoints.
   breakpoints({
@@ -52,52 +41,5 @@
 
   breakpoints.on(">medium", function () {
     $footer.appendTo($header);
-  });
-
-  // Header.
-
-  // Parallax background.
-
-  // Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-  if (browser.name == "ie" || browser.mobile) settings.parallax = false;
-
-  if (settings.parallax) {
-    breakpoints.on("<=medium", function () {
-      $window.off("scroll.strata_parallax");
-      $header.css("background-position", "");
-    });
-
-    breakpoints.on(">medium", function () {
-      $header.css("background-position", "left 0px");
-
-      $window.on("scroll.strata_parallax", function () {
-        $header.css("background-position", "left " + -1 * (parseInt($window.scrollTop()) / settings.parallaxFactor) + "px");
-      });
-    });
-
-    $window.on("load", function () {
-      $window.triggerHandler("scroll");
-    });
-  }
-
-  // Main Sections: Two.
-
-  // Lightbox gallery.
-  $window.on("load", function () {
-    $("#two").poptrox({
-      caption: function ($a) {
-        return $a.next("h3").text();
-      },
-      overlayColor: "#2c2c2c",
-      overlayOpacity: 0.85,
-      popupCloserText: "",
-      popupLoaderText: "",
-      selector: ".work-item a.image",
-      usePopupCaption: true,
-      usePopupDefaultStyling: false,
-      usePopupEasyClose: false,
-      usePopupNav: true,
-      windowMargin: breakpoints.active("<=small") ? 0 : 50,
-    });
   });
 })(jQuery);
