20bf260c4081cf677c87e18ea8cd3459e5ad36b7
Christopher Edelmaier <cedelmaier@flatironinstitute.org>
2023-09-19 14:11:33 -0400

    28: adding information to first slurm script

--- 28_CCB/mpi_omp_mockup/run_slurm_example1.sh
+++ 28_CCB/mpi_omp_mockup/run_slurm_example1.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=8
-#SBATCH --cpus-per-task=16
-#SBATCH --constraint=rome,ib
-#SBATCH --partition=ccb
-#SBATCH --time=00:10:00
-#SBATCH --job-name=mpi_omp_example1
-#SBATCH --output=mpi_omp_example1.log
+#SBATCH --nodes=2                     # number of nodes
+#SBATCH --ntasks-per-node=8           # number of tasks per node
+#SBATCH --cpus-per-task=16            # number of cpus(threads) per task
+#SBATCH --constraint=rome,ib          # computers to use at FI
+#SBATCH --partition=ccb               # center allocation
+#SBATCH --time=00:10:00               # maximum time for job
+#SBATCH --job-name=mpi_omp_example1   # job name
+#SBATCH --output=mpi_omp_example1.log # slurm output file
 
 # Set up our environment for this SLURM submission
-module -q purge
-module -q load openmpi
-module list
+module -q purge                       # purge current modules
+module -q load openmpi                # Load openmpi
+module list                           # What modules are loaded?
 
 # Helper functions to see what kind of system we are running on, if we have GPUs that are accessible, and other information
-lscpu
-nvidia-smi
-numactl -H
+lscpu                                 # What cpus do we have?
+nvidia-smi                            # Is there gpu information?
+numactl -H                            # What is the NUMA layout
 
 # Print some helpful information
 echo "Slurm nodes:              ${SLURM_NNODES}"
